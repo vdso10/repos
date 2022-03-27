37ed8f33f0e93d84b5c41d20237e14bc26002816
vdso10 <53046799+vdso10@users.noreply.github.com>
2022-03-27 19:22:03 -0300

    Commit 2 - Estilizando o formulario

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import {FaGithub, FaPlus } from 'react-icons/fa'
 
-import { Title } from './styles'
+import { Container, Form, SubmitButton } from './styles'
 
 const Main = () => {
     return (
-      <Title>
-        Main
-      </Title>
+
+      <Container>
+
+        <h1>
+          <FaGithub size={25} />
+          Meus Repositorios
+        </h1>
+
+        <Form>
+          <input type='text' placeholder='Adicionar Repositorios' />
+
+          <SubmitButton>
+            
+            <FaPlus color='#FFF' size={14} />
+
+          </SubmitButton>
+        </Form>
+        
+      </Container>
     );
   }
   
